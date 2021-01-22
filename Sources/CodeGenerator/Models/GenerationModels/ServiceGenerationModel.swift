8a7080f4bde53ec2904ba0f7e4676ce2da2681a1
Dmitry Demyanov <dmitryd20@gmail.com>
2021-01-22 15:29:15 +0300

    added optional signs to templates

--- Sources/CodeGenerator/Models/GenerationModels/ServiceGenerationModel.swift
+++ Sources/CodeGenerator/Models/GenerationModels/ServiceGenerationModel.swift
@@ -12,9 +12,11 @@ public struct ServiceGenerationModel {
 
     public let name: String
     public let paths: [PathModel]
+    public let codingKeys: [String]
 
     public init(name: String, paths: [PathModel]) {
         self.name = name
         self.paths = paths.sorted { $0.name < $1.name }
+        self.codingKeys = paths.flatMap { $0.codingKeys }
     }
 }
